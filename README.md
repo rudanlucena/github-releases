0c4189c85da5e9c3530ae706a4f8bbc97fcc6097
rudanlucena <rudanlucena@hotmail.com>
2017-12-17 22:56:24 -0300

    add projeto inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# github releases
+
+projeto de integração do github releases com o travis ci
---
+++ src/main/java/br/edu/ifpb/pw2/githubreleases/App.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pw2.githubreleases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
